e6e1d4a3629bdbd04f69fd90389f5bf8237749a7
bakery <lewis@osas.solutions>
2026-09-12 01:47:50 +0100

    Make vout.n in Transaction optional

---
+++ prisma/migrations/20260912004740_make_vout_n_optional_lol/migration.sql
@@ -0,0 +1,21 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Transaction" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "derivation" INTEGER NOT NULL,
+    "address" TEXT NOT NULL,
+    "label" TEXT,
+    "amount_sats" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+    "created" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "txid" TEXT,
+    "vout_n" INTEGER
+);
+INSERT INTO "new_Transaction" ("address", "amount_sats", "created", "derivation", "id", "label", "status", "txid", "vout_n") SELECT "address", "amount_sats", "created", "derivation", "id", "label", "status", "txid", "vout_n" FROM "Transaction";
+DROP TABLE "Transaction";
+ALTER TABLE "new_Transaction" RENAME TO "Transaction";
+CREATE UNIQUE INDEX "Transaction_derivation_key" ON "Transaction"("derivation");
+CREATE UNIQUE INDEX "Transaction_address_key" ON "Transaction"("address");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
